8b98559254db0f537a9a04d2c4f281a497d6c141
Jeyi Adole <meetjeyvers@gmail.com>
2025-06-12 14:35:24 +0100

    our first contract

--- solidity/cyfrin-solidity-101/SimpleStorage.sol
+++ solidity/cyfrin-solidity-101/SimpleStorage.sol
@@ -3,15 +3,13 @@ pragma solidity ^0.8.18; // stating our version
 
 
 contract SimpleStorage {
-    // Basic Types: boolean, uint, int, address, bytes
-    bool hasFavoriteNumber = false;
-    uint numberOfBytes = 88;
-    uint256 favoriteNumber = 8;
-    // strings are bytes objects as text
-    string favoriteNumberInText = "this is my favorite number in test: eight";
-    int256 favoriteInt = -2;
-    address myAddress = 0xE9b2F5394eE76F5ef758541A6687FfdA4c3EC0CA;
-    bytes32 favoriteBytes = "dog";
-    // what is the difference between int and uint?
+    // if you don't give a visibility specifier to your variables, it default to internal
+    uint256 public favoriteNumber = 72; // default value is 0
 
+    // this will be responsible for updating our favoriteNumber
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
 }
+
+// 0xd9145CCE52D386f254917e481eB44e9943F39138
