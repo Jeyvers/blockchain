15bd8f0a4ab2b9a19a0231f791e08fb6acde49b7
Jeyi Adole <meetjeyvers@gmail.com>
2025-06-12 21:20:11 +0100

    working with types

--- solidity/cyfrin-solidity-101/SimpleStorage.sol
+++ solidity/cyfrin-solidity-101/SimpleStorage.sol
@@ -3,18 +3,26 @@ pragma solidity ^0.8.18; // stating our version
 
 
 contract SimpleStorage {
-    // if you don't give a visibility specifier to your variables, it default to internal
-    uint256 public favoriteNumber = 72; // default value is 0
+    // if you don't give a visibility specifier to your variables, it default to internal which means they can't be asessed by the public
+    uint256 myFavoriteNumber = 72; // default value is 0
 
-    // this will be responsible for updating our favoriteNumber
-    function store(uint256 _favoriteNumber) public {
-        favoriteNumber = _favoriteNumber;
+    // structs are just the way types are created in solidity. just see a struct as a type
+    struct Person {
+        string name;
+        uint256 favoriteNumber;
+    }
+
+    Person public Jeyi = Person("Jeyi", 8);
+
+    // this will be responsible for updating our myFavoriteNumber
+    function store(uint256 favoriteNumber) public {
+        myFavoriteNumber = favoriteNumber;
     }
 
     // view functions disallow updating of state, 
     // pure functions disallow reading of state
     function retrieve() public view returns(uint256) {
-        return favoriteNumber;
+        return myFavoriteNumber;
     }
 }
 
